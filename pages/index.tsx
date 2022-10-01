e5f921c6ea5cba35408543319980562c60ea0d1e
olaoluwa vincent <olaoluwavincent@gmail.com>
2022-10-01 02:35:22 +0100

    cool

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,7 +33,6 @@ type Props = {
 };
 
 const Home = ({ experiences, pageInfo, projects, skills, socials }: Props) => {
-	console.log(socials);
 	return (
 		<div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory z-0 overflow-y-scroll overflow-x-hidden scrollbar-thin scrollbar-thumb-[#F7AB0A] scrollbar-track-gray-400/20'>
 			<Head>
